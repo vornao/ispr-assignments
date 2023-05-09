a157e114bfcc7cfc6e1fbcc212a1104f1feac61a
Luca <migliorluca@gmail.com>
2023-05-09 12:49:24 +0200

    updated images

--- assignment-three/conv.py
+++ assignment-three/conv.py
@@ -111,3 +111,50 @@ def test(model, device, test_loader, criterion, epoch, verbose=False):
 
     return epoch_loss/len(test_loader), correct/len(test_loader.dataset)
 
+class BlockCNN(nn.Module):
+    # define sequential model
+    def __init__(self):
+        super(BlockCNN, self).__init__()
+
+        # first convolutional layers
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)              # padding to keep size
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)             
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)            
+        self.conv4 = nn.Conv2d(128, 256, 3)                      # no padding
+        # MLP classifier
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+        self.softmax = nn.LogSoftmax(dim=1)
+        self.relu = nn.ReLU()
+        
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.dropout = nn.Dropout(p=0.3)
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+
+        # first block
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.dropout(self.pool(x))
+
+        # second block
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.dropout(self.pool(x))
+
+        # third block
+        x = self.relu(self.bn3(self.conv3(x)))
+        x = self.dropout(self.pool(x))
+
+        # fourth block
+        x = self.relu(self.bn4(self.conv4(x)))
+        x = self.dropout(self.pool(x))
+        
+        x = torch.flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.softmax(self.fc2(x))
+        return x
